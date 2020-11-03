05920c0816080888b517879baaed7800c43be995
jazielloureiro <jazielloureiro17@gmail.com>
2020-11-03 19:10:41 -0300

    Add insufficient material final to endgame.h

--- source/endgame.h
+++ source/endgame.h
@@ -3,6 +3,7 @@
 
 bool verify_check(square board[][BOARD_SIZE], char move);
 bool is_there_threefold_repetition(History *history);
+bool is_there_insufficient_material(square board[][BOARD_SIZE]);
 bool special_finals(square board[][BOARD_SIZE], History *history);
 
 #endif
