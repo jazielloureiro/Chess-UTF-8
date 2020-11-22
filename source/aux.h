b97c762373c71b8779b1dc710bb04407193124e4
jazielloureiro <jazielloureiro17@gmail.com>
2020-11-21 22:20:56 -0300

    Improve headers organization

--- source/aux.h
+++ source/aux.h
@@ -1,8 +1,14 @@
 #ifndef AUX_H
 #define AUX_H
 
+#include "chess.h"
+
 void clear_input_buffer();
 void clear_screen();
 void pause_screen();
+void print_board(square board[][BOARD_SIZE]);
+void print_error_message(int message);
+void print_final_board(square board[][BOARD_SIZE], char final);
+void print_top_menu(char turn, bool check);
 
 #endif
